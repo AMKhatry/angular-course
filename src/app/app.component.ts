a4a7c719a167087cb2ab50902edf8812b505c464
Aditya Khatry <adityak@lims1.com>
2023-08-25 14:25:07 +1000

    View Children Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +13,32 @@ export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
-  @ViewChild('cardRef1', {read: ElementRef}) 
-  card1: CourseCardComponent;
 
-  @ViewChild ('courseCardImage')
-  courseCardImage: ElementRef;
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards : QueryList<ElementRef>;
 
   constructor(){
 
   }
   
   ngAfterViewInit() {
-      console.log("courseCardImage",this.courseCardImage);
 
+      console.log(this.cards); 
+
+  }
+
+  onCoursesEdited(){
+    this.courses.push(
+      {
+        id: 1,
+        description: "Angular core deep dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        //category: 'INTERMEDIATE',
+        lessonsCount: 10
+      }
+
+    );
   }
 
   onCourseSelected(course:Course){
