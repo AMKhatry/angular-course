6d6d7995fe674ff9a6522cf8f954253497d4c2d2
Aditya Khatry <adityak@lims1.com>
2023-08-25 15:02:37 +1000

    Angular Content Projection with ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,31 +14,13 @@ export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
 
-  @ViewChildren(CourseCardComponent, {read: ElementRef})
-  cards : QueryList<ElementRef>;
 
   constructor(){
 
   }
   
   ngAfterViewInit() {
-
-      console.log(this.cards); 
-
-  }
-
-  onCoursesEdited(){
-    this.courses.push(
-      {
-        id: 1,
-        description: "Angular core deep dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
-        //category: 'INTERMEDIATE',
-        lessonsCount: 10
-      }
-
-    );
+ 
   }
 
   onCourseSelected(course:Course){
